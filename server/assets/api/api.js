8429629c7fd62257e6b07a9a0ccde6658d7c39d8
Bastien <bastien.hugon@epitech.eu>
2018-03-21 11:15:46 +0000

    [Server] Adding  listener

--- server/assets/api/api.js
+++ server/assets/api/api.js
@@ -2,4 +2,8 @@ io.on('connection', function (socket) {
     var ip = socket.handshake.headers["x-real-ip"];
     var port = socket.handshake.headers["x-real-port"];
     console.log("Connection from: " + ip + ":" + port);
+
+    socket.on('login', function(mail, password){
+	socket.emit('login', 'OK');
+    });
 });
