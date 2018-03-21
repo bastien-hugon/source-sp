bf392419cf4860158ba7ea0e98fe177d121344fd
Bastien <bastien.hugon@epitech.eu>
2018-03-21 16:10:40 +0000

    [Server] Adding mongodb connexion

--- server/assets/api/api.js
+++ server/assets/api/api.js
@@ -29,7 +29,7 @@ io.on('connection', function (socket) {
 			var dbo = db.db("simply");
 			var hash = crypto.createHash('sha256').update(pwd).digest('base64');
 			console.log(hash);
-			dbo.collection("users").find({}, { mail: mail, password: hash }).toArray(function(err, res) {
+			dbo.collection("users").find({}, { _id: 0, mail: mail, password: hash }).toArray(function(err, res) {
 				if (err) throw err;
 				db.close();
 				console.log(res);
