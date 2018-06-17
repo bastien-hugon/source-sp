3d1559dee4089591634a0e22f05628ff3730679e
Bastien Hugon <bastien.hugon@epitech.eu>
2018-06-17 16:44:14 +0200

    Adding getShared function

--- server/assets/api/api.js
+++ server/assets/api/api.js
@@ -211,7 +211,7 @@ io.on('connection', function (socket) {
 	});
 	
 	socket.on('getShared', function(token, dir){
-		console.log(JSON.stringify(data));
+		console.log(JSON.stringify(data[dir]));
 		MongoClient.connect(url, function(err, db) {
 			if (err) throw err;
 			var dbo = db.db("simply");
